e7b582539baa20d3c9b2c629eea4702454e1dc0e
david-huynh <huynh.david.work@gmail.com>
2020-09-07 21:35:35 -0400

    Updated camera screen

--- ontrack/CameraScreen.js
+++ ontrack/CameraScreen.js
@@ -1,17 +1,71 @@
 import React from 'react';
-
+import { RNCamera } from 'react-native-camera';
+import { withNavigationFocus } from 'react-navigation';
 import {
-    SafeAreaView,
-    StyleSheet,
-    View,
-    Text,
-    StatusBar,
+  AppRegistry,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity
 } from 'react-native';
 
 const CameraScreen = () =>{
+  takePicture = async () => {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
   return (
-    <Text></Text>
+    <View style={styles.container}>
+      <RNCamera
+        ref={ref => {
+          this.camera = ref;
+        }}
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        captureAudio={false}
+      />
+      <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+        <TouchableOpacity onPress={takePicture.bind(this)} style={styles.capture}>
+          <Text style={{ fontSize: 14 }}> SNAP </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
+
+  
 }
 
-export default CameraScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
+
+export default withNavigationFocus(CameraScreen);
